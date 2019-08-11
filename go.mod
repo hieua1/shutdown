1f712c9337377aa19e1b5648182d8a96c0b01f4d
tuedt <v.tuedt1@vinid.net>
2019-08-11 17:43:43 +0700

    first version - sigterm handler

---
+++ go.mod
@@ -0,0 +1,8 @@
+module github.com/hieua1/shutdown
+
+go 1.12
+
+require (
+	github.com/hieua1/logger v0.0.0-20190811100313-17c5c2133d4f
+	github.com/hieua1/observer v1.0.0
+)
---
+++ sighandler.go
@@ -0,0 +1,62 @@
+package shutdown
+
+import (
+	"os"
+	"os/signal"
+	"sync"
+	"syscall"
+	"time"
+
+	"github.com/hieua1/logger"
+	"github.com/hieua1/observer"
+)
+
+type SigtermHandler interface {
+	RegisterDeferFunc(func())
+}
+
+type sigtermHandler struct {
+	observer.Subject
+	sigChannel chan os.Signal
+	timeout    time.Duration
+}
+
+var (
+	sigtermHdl     *sigtermHandler
+	sigHdlInitOnce sync.Once
+)
+
+func GetSigtermHandler() SigtermHandler {
+	sigHdlInitOnce.Do(func() {
+		sigtermHdl = &sigtermHandler{
+			Subject:    new(observer.BaseSubject),
+			sigChannel: make(chan os.Signal, 1),
+			timeout:    -1,
+		}
+		signal.Notify(sigtermHdl.sigChannel, os.Interrupt, syscall.SIGTERM)
+		go func() {
+			select {
+			case s := <-sigtermHdl.sigChannel:
+				logger.S().Info("Receive signal: ", s)
+				if sigtermHdl.timeout > 0 {
+					go func() {
+						select {
+						case <-time.After(sigtermHdl.timeout):
+							logger.S().Info("Timeout! Force application to exit.")
+							os.Exit(1)
+						}
+					}()
+				}
+				logger.S().Info("Waiting for gracefully finishing current works before shutdown...")
+				sigtermHdl.NotifyAll(nil)
+			}
+		}()
+	})
+	return sigtermHdl
+}
+
+func (s *sigtermHandler) RegisterDeferFunc(f func()) {
+	s.RegisterObserver(observer.Func(func(data interface{}) {
+		f()
+	}))
+}
