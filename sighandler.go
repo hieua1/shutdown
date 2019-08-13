9d706af250c8e6107a878e5bf741e383f514c92d
tuedt <v.tuedt1@vinid.net>
2019-08-13 19:49:01 +0700

    use closure

--- sighandler.go
+++ sighandler.go
@@ -22,37 +22,42 @@ type sigtermHandler struct {
 }
 
 var (
-	sigtermHdl     *sigtermHandler
-	sigHdlInitOnce sync.Once
+	GetSigtermHandler = getSigtermHandlerFunc()
 )
 
-func GetSigtermHandler() SigtermHandler {
-	sigHdlInitOnce.Do(func() {
-		sigtermHdl = &sigtermHandler{
-			Subject:    new(observer.BaseSubject),
-			sigChannel: make(chan os.Signal, 1),
-			timeout:    -1,
-		}
-		signal.Notify(sigtermHdl.sigChannel, os.Interrupt, syscall.SIGTERM)
-		go func() {
-			select {
-			case s := <-sigtermHdl.sigChannel:
-				logger.S().Info("Receive signal: ", s)
-				if sigtermHdl.timeout > 0 {
-					go func() {
-						select {
-						case <-time.After(sigtermHdl.timeout):
-							logger.S().Info("Timeout! Force application to exit.")
-							os.Exit(1)
-						}
-					}()
-				}
-				logger.S().Info("Waiting for gracefully finishing current works before shutdown...")
-				sigtermHdl.NotifyAll(nil)
+func getSigtermHandlerFunc() func() SigtermHandler {
+	var (
+		sigtermHdl     *sigtermHandler
+		sigHdlInitOnce sync.Once
+	)
+	return func() SigtermHandler {
+		sigHdlInitOnce.Do(func() {
+			sigtermHdl = &sigtermHandler{
+				Subject:    new(observer.BaseSubject),
+				sigChannel: make(chan os.Signal, 1),
+				timeout:    -1,
 			}
-		}()
-	})
-	return sigtermHdl
+			signal.Notify(sigtermHdl.sigChannel, os.Interrupt, syscall.SIGTERM)
+			go func() {
+				select {
+				case s := <-sigtermHdl.sigChannel:
+					logger.S().Info("Receive signal: ", s)
+					if sigtermHdl.timeout > 0 {
+						go func() {
+							select {
+							case <-time.After(sigtermHdl.timeout):
+								logger.S().Info("Timeout! Force application to exit.")
+								os.Exit(1)
+							}
+						}()
+					}
+					logger.S().Info("Waiting for gracefully finishing current works before shutdown...")
+					sigtermHdl.NotifyAll(nil)
+				}
+			}()
+		})
+		return sigtermHdl
+	}
 }
 
 func (s *sigtermHandler) RegisterDeferFunc(f func()) {
